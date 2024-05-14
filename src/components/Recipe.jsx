a290fdf83e236ebe29529c96283fe62374c4f207
YagoPazos <yagopb@gmail.com>
2024-05-14 18:59:12 +0200

    render img from contentful

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -7,10 +7,13 @@ function Recipe({ data }) {
     <div>
       <h2></h2>
       <div></div>
-      {data.map((receta, index) => (
-        <h1 className="text-black text-center" key={index}>
-          {receta.fields.title}
-        </h1>
+      {data.map((recipe, index) => (
+        <div>
+          <img src={recipe.fields.image.fields.file.url} alt={recipe.fields.title} width='2048' height='1365' />
+          <h1 className="text-black text-center text-4xl" key={recipe.sys.id}>
+            {recipe.fields.title}
+          </h1>
+        </div>  
       ))}
     </div>
   );
