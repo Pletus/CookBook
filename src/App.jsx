c52332185007ac7029ebffcf775cb8b171d1a411
YagoPazos <yagopb@gmail.com>
2024-05-14 17:58:22 +0200

    stable fetching from contentful

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Footer from './components/Footer'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import Recipe from "./components/Recipe";
+import { useState, useEffect } from "react";
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: "pr9cy2bmpopx",
+  accessToken: "a_BYJ3l6XTHr9msqs8m8UXVCSmlEeqfZcjtP14YqFGo",
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({});
+  console.log(data);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await client.getEntries({});
+        setData(response.items);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    console.log(data);
+    fetchData();
+  }, []);
 
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col">
       <div>
         <Navbar />
       </div>
       <div>
         <Hero />
       </div>
-      <div className='pl-28'>  
+      <div className="pl-28">
         <Footer />
       </div>
+      <div>
+        <Recipe />
+        <h1>{}</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Recipe.jsx
@@ -0,0 +1,13 @@
+function Recipe() {
+  console.log();
+  return (
+    <div>
+      <h2></h2>
+      <div></div>
+      <h1 className="text-black text-center">QUE COSAS</h1>
+      {/* <h1 className="text-black text-center">{recipe.fields.title}</h1> */}
+    </div>
+  );
+}
+
+export default Recipe;
