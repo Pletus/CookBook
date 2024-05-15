b042e6a0c3e8da2a6bb78bee258ee02b9f868ece
YagoPazos <yagopb@gmail.com>
2024-05-15 17:02:39 +0200

    added fetching from contentful

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Footer from './components/Footer'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: "pr9cy2bmpopx",
+  accessToken: "a_BYJ3l6XTHr9msqs8m8UXVCSmlEeqfZcjtP14YqFGo",
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await client.getEntries({});
+        setData(response.items);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+    console.log(data);
+  }, []);
 
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col">
       <div>
         <Navbar />
       </div>
       <div>
         <Hero />
       </div>
-      <div className='pl-28'>  
+      <div className="pl-28">
         <Footer />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
