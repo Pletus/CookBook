c832865c7054eab4061c6e73225612fd49665380
rbksy <rabiaaksoy93@gmail.com>
2024-05-15 16:58:12 +0200

    main

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Main from './components/Main'
 import Footer from './components/Footer'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       </div>
       <div>
         <Hero />
+        <Main />
       </div>
       <div className='pl-28'>  
         <Footer />
